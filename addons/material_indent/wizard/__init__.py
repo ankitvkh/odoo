9f1a7dd9771956688d4d12558b3869a6f678e447
PK <132288559+codewithkp@users.noreply.github.com>
2025-12-03 13:23:55 +0530

    Revert "Anish kumar 09 patch 1"

--- addons/material_indent/wizard/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import material_indent_import
--- addons/user_access_management/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import location_master
-from . import res_users
-from . import sale_order
-from . import purchase_order
-from . import stock_move
