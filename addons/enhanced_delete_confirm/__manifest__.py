7fbfd4115dd448aa3c2cc070335d9667239a929c
Anish <anishku9202@gmail.com>
2025-12-13 21:50:00 +0530

    fixed version

--- addons/enhanced_delete_confirm/__manifest__.py
+++ addons/enhanced_delete_confirm/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Enhanced Delete Confirmation',
-    'version': '17.0.1.0.0',
+    'version': '17.0',
     'category': 'Tools',
     'summary': 'Enhanced delete with unlink or cascade options',
     'description': """
