040b21d35e23b66a4150eafcec9b5ce9eaab12fe
rohitkumar <rohit02bits@gmail.com>
2025-12-18 12:23:27 +0530

    fixes

--- setup-scripts/backup-restore/backup_to_gcp_bucket.py
+++ setup-scripts/backup-restore/backup_to_gcp_bucket.py
@@ -1,16 +1,16 @@
 import os
 import datetime
 
-POSTGRES_USER = "odoo_user_new"
-POSTGRES_PASSWORD = "odoo123"
-DATABASE_NAME = "odoo_new"
-BUCKET_NAME = "your-gcs-bucket-name"  # Replace with gcp bucket name
-TEMP_DIR = "/tmp/odoo_backup"
+POSTGRES_USER = "erp_user"
+POSTGRES_PASSWORD = "powertek123"
+DATABASE_NAME = "erp_db"
+BUCKET_NAME = "erp-databackup"  # Replace with gcp bucket name
+TEMP_DIR = "/tmp/erp_backup"
 
 os.makedirs(TEMP_DIR, exist_ok=True)
 
 TIMESTAMP = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-DUMP_PATH = os.path.join(TEMP_DIR, f"odoo_backup_{TIMESTAMP}.dump")
+DUMP_PATH = os.path.join(TEMP_DIR, f"erp_backup_{TIMESTAMP}.dump")
 
 os.environ["PGPASSWORD"] = POSTGRES_PASSWORD
 
@@ -20,7 +20,7 @@ if os.system(f'pg_dump -U {POSTGRES_USER} -h localhost -F c -b -v -f "{DUMP_PATH
     exit(1)
 
 print("Uploading to GCS...")
-if os.system(f'gsutil cp "{DUMP_PATH}" gs://{BUCKET_NAME}/odoo_backups/') != 0:
+if os.system(f'gsutil cp "{DUMP_PATH}" gs://{BUCKET_NAME}/erp_backups/') != 0:
     print("Upload failed.")
     exit(1)
 
