f5d5f4be5167f5779ff62f7994928324c03acda6
rohitkumar <rohit02bits@gmail.com>
2026-01-10 11:21:55 +0530

    sequence upd

---
+++ addons/reset_sequences.sql
@@ -0,0 +1,17 @@
+-- PSQL script to reset Odoo sequences for custom Sale and Purchase Orders to 1300
+
+-- Reset Purchase Order Custom Sequence
+UPDATE ir_sequence 
+SET number_next = 1300 
+WHERE code = 'purchase.order.custom';
+
+-- Reset Sale Order Custom Sequence
+UPDATE ir_sequence 
+SET number_next = 1300 
+WHERE code = 'sale.order.custom';
+
+-- If use_advisory_lock is true, we might need to reset the actual postgres sequence too
+-- However, Odoo's next_by_code usually manages this via the ir_sequence table if implementation is 'standard'
+
+-- To see the effect, you can run:
+-- SELECT name, code, number_next FROM ir_sequence WHERE code IN ('purchase.order.custom', 'sale.order.custom');
